0af12efa933764e9f60ddac37131416e676e4081
E.O.R_BAH545 <elle.oumarrafiou@gmail.com>
2025-09-11 21:16:05 +0000

    Create variables.js

---
+++ examples/basics/variables.js
@@ -0,0 +1,8 @@
+let prenom = "Alice";
+const age = 25;
+var estEtudiant = true;
+let hobbies = ["lecture", "jeux", "sport"];
+let utilisateur = { nom: "Bob", age: 30 };
+console.log(prenom);
+console.log(hobbies[0]);
+console.log(utilisateur.nom);
