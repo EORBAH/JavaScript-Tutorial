4c4e885abfbb61a63f7818b9fc5088b4efa2e589
E.O.R_BAH545 <elle.oumarrafiou@gmail.com>
2025-09-11 21:15:06 +0000

    Create control_structures.js

---
+++ examples/basics/control_structures.js
@@ -0,0 +1,12 @@
+let note = 85;
+if (note >= 90) {
+    console.log("Excellent");
+} else if (note >= 70) {
+    console.log("Bien");
+} else {
+    console.log("À améliorer");
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
